582073c66c9e417fcdaeb41b1a3281cd9619fcd5
Zubair <zubair99ss@hotmail.com>
2024-01-24 21:35:23 +0800

    Complete the javascript file

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const equalsBtn = document.getElementById('equals')
 
 periodBtn.addEventListener('click', onDecimalPressed);
 clearBtn.addEventListener('click', onClear);
-// equalsBtn.addEventListener('click', evaluate);
+equalsBtn.addEventListener('click', evaluate);
 
 operators.forEach((button) =>
     button.addEventListener('click', () => onOperationPressed(button.textContent, shouldResetScreen))
@@ -60,3 +60,62 @@ function onClear() {
     shouldResetScreen = false;
 }
 
+// Take input from displayscreen.textContent and split the operators and numbers
+function parseCalculationString() {
+    // Use regular expression to match numbers and operands
+    var regex = /(\d+|\+|\-|\x|\/)/g;
+    var matches = displayScreen.textContent.match(regex);
+
+    // Convert matches to an array of numbers and operands
+    var result = matches.map(function (match) {
+        return isNaN(match) ? match : parseFloat(match);
+    });
+    return result;
+}
+
+function calculateMulDiv(expressionArray) {
+    for (var i = 0; i < expressionArray.length; i++) {
+        if (expressionArray[i] === 'x') {
+            expressionArray[i - 1] = expressionArray[i - 1] * expressionArray[i + 1];
+            expressionArray.splice(i, 2); // Remove the operator and the next number
+            i--; // Decrement index to reevaluate the current position
+        } else if (expressionArray[i] === '/') {
+            expressionArray[i - 1] = expressionArray[i - 1] / expressionArray[i + 1];
+            expressionArray.splice(i, 2); // Remove the operator and the next number
+            i--; // Decrement index to reevaluate the current position
+        }
+    }
+
+    return expressionArray;
+}
+
+function calculateAddSub(expressionArray) {
+    for (var j = 0; j < expressionArray.length; j++) {
+        if (expressionArray[j] === '+') {
+            expressionArray[j - 1] = expressionArray[j - 1] + expressionArray[j + 1];
+            expressionArray.splice(j, 2); // Remove the operator and the next number
+            j--; // Decrement index to reevaluate the current position
+        } else if (expressionArray[j] === '-') {
+            expressionArray[j - 1] = expressionArray[j - 1] - expressionArray[j + 1];
+            expressionArray.splice(j, 2); // Remove the operator and the next number
+            j--; // Decrement index to reevaluate the current position
+        }
+    }
+    return expressionArray;
+}
+
+function evaluate(equation) {
+    let inputString = equation.toString();
+    console.log(inputString)
+    var resultArray = parseCalculationString(inputString);
+
+    // First, perform multiplication and division
+    resultArray = calculateMulDiv(resultArray);
+
+    // Then, perform addition and subtraction on the updated array
+    var finalResult = calculateAddSub(resultArray);
+
+    console.log(finalResult[0]);
+    let resultString = finalResult[0];
+    displayScreen.textContent = resultString;
+}
